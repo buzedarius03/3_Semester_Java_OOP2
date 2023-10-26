536e04523bbec4d616d11b6288358c5623d4ba7e
Darius Buze <darius.buze@student.tugraz.at>
2023-10-26 16:11:05 +0200

    first try of the environmental arguments

--- backend/src/main/java/at/tugraz/oop2/MapServiceServer.java
+++ backend/src/main/java/at/tugraz/oop2/MapServiceServer.java
@@ -4,8 +4,23 @@ import java.util.logging.Logger;
 
 public class MapServiceServer {
     private static final Logger logger = Logger.getLogger(MapServiceServer.class.getName());
- 
+
     public static void main(String[] args) {
         logger.info("Starting backend...");
+        String port = "JMAP_BACKEND_PORT";
+        String file = "JMAP_BACKEND_OSMFILE";
+        String JMAP_BACKEND_OSM_FILE_STRING = System.getenv().getOrDefault(file, "data/styria_reduced.osm");
+        String JMAP_BACKEND_PORT_String = System.getenv().getOrDefault(port, "8020");
+
+        int JMAP_BACKEND_PORT;
+        try {
+            JMAP_BACKEND_PORT = Integer.parseInt(JMAP_BACKEND_PORT_String);
+        }
+        catch(NumberFormatException e) {
+            JMAP_BACKEND_PORT = 8020;
+        }
+
+        MapLogger.backendStartup(JMAP_BACKEND_PORT, JMAP_BACKEND_OSM_FILE_STRING);
+        //System.out.println(JMAP_BACKEND_PORT);
     }
 }
--- middleware/src/main/java/at/tugraz/oop2/MapApplication.java
+++ middleware/src/main/java/at/tugraz/oop2/MapApplication.java
@@ -1,5 +1,21 @@
 package at.tugraz.oop2;
 public class MapApplication {
     public static void main(String[] args) {
+        String port = "JMAP_MIDDLEWARE_PORT";
+        String target = "JMAP_BACKEND_TARGET";
+        String JMAP_MIDDLEWARE_PORT_String = System.getenv().getOrDefault(port, "8010");
+        String JMAP_BACKEND_TARGET_String = System.getenv().getOrDefault(target, "localhost: 8020");
+
+        int JMAP_MIDDLEWARE_PORT;
+        try {
+            JMAP_MIDDLEWARE_PORT = Integer.parseInt(JMAP_MIDDLEWARE_PORT_String);
+        }
+        catch(NumberFormatException e) {
+            JMAP_MIDDLEWARE_PORT = 8010;
+        }
+
+        MapLogger.middlewareStartup(JMAP_MIDDLEWARE_PORT, JMAP_BACKEND_TARGET_String);
+
+        //System.out.println(JMAP_MIDDLEWARE_PORT);
     }
 }
