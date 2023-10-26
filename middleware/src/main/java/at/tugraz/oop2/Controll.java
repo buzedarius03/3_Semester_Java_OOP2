af848aa84d25165f7a6a446831f009d2cd76c830
Darius Buze <darius.buze@student.tugraz.at>
2023-10-26 22:55:41 +0200

    second try of the requests

--- middleware/src/main/java/at/tugraz/oop2/Controll.java
+++ middleware/src/main/java/at/tugraz/oop2/Controll.java
@@ -1,6 +1,7 @@
 package at.tugraz.oop2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -16,6 +17,30 @@ public class Controll {
         Map<String, Integer> paging_entry = Map.ofEntries(entry("skip", 0), entry("take", 1), entry("total", 6));
         return Map.ofEntries(entry("entries",  new Amenities[]{Amenity}), entry("paging", paging_entry));
     }
+
+    @GetMapping("/amenities/{amenity_id}")
+    public Amenities returnAmenitiesById(@PathVariable("amenity_id") long amenity_id)
+    {
+        Amenities amenity_by_ID = new Amenities(amenity_id);
+        return amenity_by_ID;
+    }
+
+    @GetMapping("/roads")
+    public Map<String, Object> returnRoadsRequest()
+    {   Roads Road = new Roads();
+        Map<String, Integer> paging_entry = Map.ofEntries(entry("skip", 0), entry("take", 1), entry("total", 6));
+        return Map.ofEntries(entry("entries",  new Roads[]{Road}), entry("paging", paging_entry));
+    }
+
+    @GetMapping("/roads/{road_id}")
+    public Roads returnRoadsById(@PathVariable("road_id") long road_id)
+    {
+        Roads road_by_ID = new Roads(road_id);
+        return road_by_ID;
+    }
+
+
+
 
 
 
--- middleware/src/main/java/at/tugraz/oop2/Roads.java
+++ middleware/src/main/java/at/tugraz/oop2/Roads.java
@@ -1,15 +1,39 @@
 package at.tugraz.oop2;
 
+import lombok.Getter;
 import org.apache.logging.log4j.util.StringMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+@Getter
 public class Roads {
 
-    private String name;
-    private long id;
-    private Geometry geom;
-    private StringMap tags;
-    private String type;
+    private String name = "Dummy_name";
+    private long id = 20603;
+    private Geometry geom = new Geometry();
+    private Map<String, String> tags = Map.ofEntries(entry("Dummy", "entry"), entry("Dummy2", "entry2"));
+    private String type = "motorway";
+    private double[] child_ids = {10 ,10};
+
+    public Roads()
+    {
+
+    }
+    public Roads(long id)
+    {
+        this.id = id;
+    }
+
+    public Roads(String name, long id, Geometry geom, Map<String, String> tags, String type, double[] child_ids) {
+        this.name = name;
+        this.id = id;
+        this.geom = geom;
+        this.tags = tags;
+        this.type = type;
+        this.child_ids = child_ids;
+    }
 
 }
